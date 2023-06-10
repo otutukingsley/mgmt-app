d6080d5223625053089bc5e48f6c0bd1fcf98a32
Otutu Chinedu Kingsley <chineduotutu3@gmail.com>
2023-06-11 00:21:53 +0100

    Modify dashboard controller

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,4 +1,23 @@
-<section class="space-y-6">
+<section class="space-y-6" x-data="{
+    showDelete: false,
+    openModal() {
+        this.showDelete = true;
+    },
+    closeModal() {
+        this.showDelete = false;
+    },
+    deleteAccount() {
+        // Perform deletion logic here
+        // You can submit the form or make an AJAX request
+        // If successful, you can redirect or show a success message
+    }
+}" x-init="$watch('showDelete', value => {
+    if (value) {
+        $dispatch('open-modal', 'confirm-user-deletion');
+    } else {
+        $dispatch('close-modal', 'confirm-user-deletion');
+    }
+})">
     <header>
         <h2 class="text-lg font-medium text-gray-900">
             {{ __('Delete Account') }}
@@ -9,13 +28,12 @@
         </p>
     </header>
 
-    <x-danger-button
-        x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    <x-danger-button x-on:click.prevent="openModal">
+        {{ __('Delete Account') }}
+    </x-danger-button>
 
-    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
-        <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
+    <x-modal name="confirm-user-deletion" trigger="showDelete" focusable>
+        <form method="post" action="{{ route('profile.destroy') }}" class="p-6" x-on:submit.prevent="deleteAccount">
             @csrf
             @method('delete')
 
@@ -30,23 +48,18 @@
             <div class="mt-6">
                 <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
 
-                <x-text-input
-                    id="password"
-                    name="password"
-                    type="password"
-                    class="mt-1 block w-3/4"
-                    placeholder="{{ __('Password') }}"
-                />
+                <x-text-input id="password" name="password" type="password" class="block w-3/4 mt-1"
+                    placeholder="{{ __('Password') }}" />
 
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
             </div>
 
-            <div class="mt-6 flex justify-end">
-                <x-secondary-button x-on:click="$dispatch('close')">
+            <div class="flex justify-end mt-6">
+                <x-secondary-button x-on:click="closeModal">
                     {{ __('Cancel') }}
                 </x-secondary-button>
 
-                <x-danger-button class="ml-3">
+                <x-danger-button type="submit" class="ml-3">
                     {{ __('Delete Account') }}
                 </x-danger-button>
             </div>
